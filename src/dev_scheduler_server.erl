12063c351866eaef0c217210d0117cc73fe2f44a
Sam Williams <sam@arweave.org>
2025-07-15 12:40:13 -0400

    fix: scheduler server uses full node message in signing flow

--- src/dev_scheduler_server.erl
+++ src/dev_scheduler_server.erl
@@ -228,6 +228,7 @@ do_assign(State, Message, ReplyPID) ->
 
 %% @doc Commit to the assignment using all of our appropriate wallets.
 commit_assignment(BaseAssignment, State) ->
+    Opts = maps:get(opts, State),
     Wallets = maps:get(wallets, State),
     Opts = maps:get(opts, State),
     lists:foldr(
